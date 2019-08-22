817f5b6bbde6b3116276c809116421b7741efd6f
john horta <john.horta@yahoo.com>
2019-08-22 12:02:55 -0400

    added items

---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  Headline: require("../models/Article"),
+  Note: require("../models/Note")
+};
---
+++ scripts/scrape.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+let scrape = function () {
+
+  return axios.get("http://www.nytimes.com").then(function (res) {
+    let $ = cheerio.load(res.data);
+    let articles = [];
+
+
+    $("article.css-180b3ld").each(function (i, element) {
+      let head = $(this)
+        .find("h2")
+        .text()
+        .trim();
+
+      let url = $(this)
+        .find("a")
+        .attr("href");
+
+      let sum = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      if (head && sum && url) {
+        let headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+        let sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+        let dataToAdd = {
+          headline: headNeat,
+          summary: sumNeat,
+          url: "https://www.nytimes.com" + url
+        };
+
+        articles.push(dataToAdd);
+      }
+    });
+    return articles;
+  });
+};
+
+
+module.exports = scrape;
