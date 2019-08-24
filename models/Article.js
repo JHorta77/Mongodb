4b09ad71287cfff855ca26f8366cd9f427f75038
john horta <john.horta@yahoo.com>
2019-08-24 09:56:01 -0400

    eight

--- models/Article.js
+++ models/Article.js
@@ -1,25 +1,25 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-let ArticleSchema = new Schema({
+// let ArticleSchema = new Schema({
 
-    title: {
-        type: String,
-        required: true
-    },
+//     title: {
+//         type: String,
+//         required: true
+//     },
 
-    link: {
-        type: String,
-        required: true
-    },
+//     link: {
+//         type: String,
+//         required: true
+//     },
 
-    note: {
-        type: Schema.Types.ObjectId,
-        ref: "Note"
-    }
-});
+//     note: {
+//         type: Schema.Types.ObjectId,
+//         ref: "Note"
+//     }
+// });
 
-const Article = mongoose.model("Article", ArticleSchema);
+// const Article = mongoose.model("Article", ArticleSchema);
 
-module.exports = Article;
+// module.exports = Article;
